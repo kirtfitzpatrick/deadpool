e52db7d4705424d5c908d32662e3b7b4069f6e32
Ben Marini <ben.marini@akqa.com>
2010-10-01 16:33:06 -0700

    Add tests for state

---
+++ test/unit/state_snapshot_test.rb
@@ -0,0 +1,7 @@
+require 'test/test_helper'
+
+class StateSnapshotTest < Test::Unit::TestCase
+  def test_case_name
+    assert "true"
+  end
+end
---
+++ test/unit/state_test.rb
@@ -0,0 +1,69 @@
+require 'test/test_helper'
+
+class StateTest < Test::Unit::TestCase
+  def setup
+    @state = Deadpool::State.new "Test"
+  end
+
+  def test_should_initialize_with_clean_state
+    assert_equal "Test", @state.name
+    assert_equal Deadpool::OK, @state.status_code
+    assert_equal [], @state.error_messages
+    assert_equal [], @state.all_messages
+  end
+
+  def test_should_set_state_to_warning
+    @state.set_state(Deadpool::WARNING, "Threat level is orange")
+
+    assert_equal Deadpool::WARNING, @state.status_code
+    assert_equal "Threat level is orange", @state.error_messages.first
+    assert_equal [], @state.all_messages
+  end
+
+  def test_should_set_state_to_ok
+    @state.set_state(Deadpool::OK, "Everything is cool")
+
+    assert_equal Deadpool::OK, @state.status_code
+    assert_equal [], @state.error_messages
+    assert_equal "Everything is cool", @state.all_messages.first
+  end
+
+  def test_should_not_set_state_when_locked
+    @state.lock
+    @state.set_state(Deadpool::WARNING, "Threat level is orange, but state is locked")
+
+    assert_equal Deadpool::OK, @state.status_code
+    assert_equal [], @state.error_messages
+    assert_equal [], @state.all_messages
+  end
+
+  def test_should_reset_state
+    @state.set_state(Deadpool::WARNING, "Threat level is orange")
+    @state.reset!
+
+    assert_equal Deadpool::OK, @state.status_code
+  end
+
+  def test_should_escalate_status_code
+    @state.escalate_status_code Deadpool::WARNING
+
+    assert_equal Deadpool::WARNING, @state.status_code
+  end
+
+  def test_should_not_escalate_to_lower_status_code
+    @state.set_state(Deadpool::CRITICAL, "Threat level is red")
+    @state.escalate_status_code Deadpool::WARNING
+
+    assert_equal Deadpool::CRITICAL, @state.status_code
+  end
+
+  def test_should_add_message
+    @state.add_message "A message"
+    assert_equal "A message", @state.all_messages.first
+  end
+
+  def test_should_add_error_message
+    @state.add_error_message "An error message"
+    assert_equal "An error message", @state.error_messages.first
+  end
+end
