6d30d7b605aa2c47f0ddab6eab6ea14236dc0ae5
Ben Marini <bmarini@gmail.com>
2010-10-03 16:21:36 -0700

    Add mocha test dependency

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,11 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'rubygems'
 require 'test/unit'
+
+begin
+  require 'mocha'
+rescue LoadError
+  abort "Install mocha with `gem install mocha`"
+end
+
 require 'deadpool'
