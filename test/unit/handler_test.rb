16143b92ec7f001ad88f43d30a10fa58406ff7b4
Ben Marini <bmarini@gmail.com>
2010-10-03 18:36:31 -0700

    Add more tests to handler

--- test/unit/handler_test.rb
+++ test/unit/handler_test.rb
@@ -2,6 +2,10 @@ require 'test/test_helper'
 
 class HandlerTest < Test::Unit::TestCase
 
+  class MockTimer
+    def cancel; end
+  end
+
   class Deadpool::Monitor::Mock < Deadpool::Monitor::Base
   end
 
@@ -11,6 +15,7 @@ class HandlerTest < Test::Unit::TestCase
   def setup
     @config = {
       :pool_name => "test.fake",
+      :max_failed_checks => 2,
       :monitor_config => { :monitor_class => "Mock" },
       :failover_protocol_configs => [
         { :protocol_class => "Mock" }
@@ -30,4 +35,21 @@ class HandlerTest < Test::Unit::TestCase
     assert_instance_of Deadpool::FailoverProtocol::Mock, @handler.instance_eval { @failover_protocols }.first
   end
 
+  def test_monitor_pool_with_normal_primary
+    monitor = @handler.instance_eval { @monitor }
+    monitor.expects(:primary_ok?).returns(true)
+
+    @handler.monitor_pool(MockTimer.new)
+    assert_equal Deadpool::OK, @handler.state.status_code
+    assert_equal 0, @handler.failure_count
+  end
+
+  def test_monitor_pool_with_failing_primary
+    monitor = @handler.instance_eval { @monitor }
+    monitor.expects(:primary_ok?).returns(false)
+
+    @handler.monitor_pool(MockTimer.new)
+    assert_equal Deadpool::WARNING, @handler.state.status_code
+    assert_equal 1, @handler.failure_count
+  end
 end
